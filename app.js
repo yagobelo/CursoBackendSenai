1c2512b7d93e7ba997069a58eba9b99be9376645
yagobelo <yago.belo00@gmail.com>
2023-04-27 22:08:29 -0300

    gitignore

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
 
-// Ler JSON
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+// Config Bodyparser
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
 
 // Rotas da API
-const usuariosRoutes = require("./routes/usuariosRoutes")
-app.use("/usuario", usuariosRoutes)
+const usuariosRoutes = require("./routes/usuariosRoutes");
+app.use("/usuario", usuariosRoutes);
 
 // Rota Inicial
-app.get('/', (req, res) => {
-  res.json({message: "Hello World!"})
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!" });
 });
 
 // Conectar com o banco de dados e com o servidor
 const PORT = process.env.PORT || 8080;
-const uri = 'mongodb://yago:dKQdYMkpowfdu2dO@ac-pn89l6z-shard-00-00.h9ghu3h.mongodb.net:27017,ac-pn89l6z-shard-00-01.h9ghu3h.mongodb.net:27017,ac-pn89l6z-shard-00-02.h9ghu3h.mongodb.net:27017/bancoCursoSenai?ssl=true&replicaSet=atlas-2v89wv-shard-0&authSource=admin&retryWrites=true&w=majority'
+const URI_DB = process.env.URI_DB;
 
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
-  console.log('Banco de dados conectado com sucesso!')
-  app.listen(PORT, () => {
-    console.log('Servidor conectado com sucesso!')
+mongoose
+  .connect(URI_DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Banco de dados conectado com sucesso!");
+    app.listen(PORT, () => {
+      console.log("Servidor conectado com sucesso!");
+    });
   })
-}).catch((err) => {
-  console.log('erro ao conectar com o banco de dados: ' + err)
-});
+  .catch((err) => {
+    console.log("erro ao conectar com o banco de dados: " + err);
+  });
